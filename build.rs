f275492c3eed1b350e1c26990648f9ba7bb3d47b
Mobin <44635056+MobinWeekend@users.noreply.github.com>
2026-07-30 02:54:28 +0330

    icon

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ fn main() {
     #[cfg(target_os = "windows")]
     {
         let mut res = winresource::WindowsResource::new();
-        res.set_icon("assets/icon.png");
+        res.set_icon("assets/icon.ico");
         res.compile().unwrap();
     }
     
